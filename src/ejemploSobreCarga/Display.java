2397eed7918cec29258942eef74d4b468136ce80
Mari <marivillaggi@gmailcom>
2017-06-08 21:11:05 -0300

    Nomina

---
+++ src/ejemploSobreCarga/Display.java
@@ -0,0 +1,43 @@
+package ejemploSobreCarga;
+
+public class Display {
+
+	
+
+	private boolean circuitos = true;
+
+	
+
+	public void pantalla( String resultado ){
+
+		
+
+		System.out.println( resultado );
+
+		
+
+	}
+
+
+
+	public boolean isCircuitos() {
+
+		return circuitos;
+
+	}
+
+
+
+	public void setCircuitos(boolean circuitos) {
+
+		this.circuitos = circuitos;
+
+	}
+
+	
+
+	
+
+
+
+}
